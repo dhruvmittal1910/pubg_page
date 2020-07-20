2c947720061f252aa6f481243ab37cba7a5a20e0
dhruv mittal <dhruvmittal1910@gmail.com>
2020-07-20 18:35:17 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.render("index");
+})
+app.get("/verify",function(req,res){
+    res.render("verify");
+})
+app.get("/details",function(req,res){
+    res.render("details");
+})
+app.get("/home",function(req,res){
+    res.render("home");
+})
+app.get("/tournDetails",function(req,res){
+    res.render("tournDetails");
+})
+app.get("/leaderboard",function(req,res){
+    res.render("leaderboard");
+})
+app.get("/prevWin",function(req,res){
+    res.render("prevWin");
+
+})
+app.get("/matchSch",function(req,res){
+    res.render("matchSch");
+})
+app.listen(process.env.PORT || 1000,function(){
+    console.log("server running on port 1000");
+    
+})
+  
